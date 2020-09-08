f0a7ad85df6fc994b326f4f9752294227437ca7f
Aaron Neslin <aneslin125@gmail.com>
2020-09-08 14:53:45 -0400

    Update Amherst orders.sql

--- Amherst orders.sql
+++ Amherst orders.sql
@@ -148,3 +148,51 @@ and substr(Z00R_FIELD_CODE,0,3) ='FMT'
     and  ord1.Z68_OPEN_DATE < 20200101
     and ord1.Z68_ORDER_STATUS  = 'SV')
     )
+    
+INTERSECT
+
+select 
+ord.Z68_ORDER_NUMBER,
+brief.z13_title,
+brief.z13_isbn_issn,
+brief.z13_author,
+brief.Z13_year,
+ord.z68_open_date,
+ord.z68_ORDER_STATUS,
+ord.z68_order_type,
+rr.z00r_text as bibFMT,
+ord.z68_material_type,
+replace(concat('AC',ord.z68_vendor_code), ' ', ''),
+ord.z68_unit_type,
+ord.z68_no_units,
+ord.z68_unit_price,
+ord.z68_total_price,
+ord.z68_library_note,
+ord.z68_vendor_note
+
+
+
+from 
+AMH50.Z68 ord
+    inner join (
+    select Z00R_DOC_NUMBER, Z00R_TEXT, substr(Z103_REC_KEY,6,9) as ADM_N from AMH50.Z103, FCL01.Z00R
+    where Z00R_DOC_NUMBER=substr(Z103_REC_KEY_1,6,9)
+    and Z103_LKR_TYPE='ADM'
+    and substr(Z103_REC_KEY_1,1,5)='FCL01'
+    and substr(Z00R_FIELD_CODE,0,3) ='FMT'
+) rr
+on substr(ord.Z68_REC_KEY ,0, 9) = rr.ADM_N
+inner join FCL01.Z13 brief
+on rr.adm_n = brief.z13_rec_key
+WHERE
+(ord.Z68_ORDER_TYPE = 'S'
+and ord.Z68_ORDER_STATUS = 'SV'
+)
+OR
+(ord.Z68_ORDER_TYPE = 'O'
+and ord.Z68_ORDER_STATUS = 'SV')
+OR
+(ord.Z68_ORDER_TYPE = 'M'
+and ord.Z68_ORDER_STATUS = 'SV'
+and ord.Z68_OPEN_DATE > 20190701)
+    
