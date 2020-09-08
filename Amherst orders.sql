33312e124eb4fb937b102deed8bdc07722b35999
Aaron Neslin <aneslin125@gmail.com>
2020-09-08 09:36:06 -0400

    added additional order material type A restriction

--- Amherst orders.sql
+++ Amherst orders.sql
@@ -109,4 +109,42 @@ and substr(Z00R_FIELD_CODE,0,3) ='FMT'
     and ord1.z68_order_type= 'M'
     and  ord1.Z68_OPEN_DATE < 20200101
     and ord1.Z68_ORDER_STATUS  = 'SV')
+ --restriction 5
+ OR
+ ord.Z68_REC_KEY not in (
+    SELECT ord1.Z68_REC_KEY FROM
+    AMH50.Z68 ord1
+    inner join (
+    select Z00R_DOC_NUMBER, Z00R_TEXT, substr(Z103_REC_KEY,6,9) as ADM_N from AMH50.Z103, FCL01.Z00R
+where Z00R_DOC_NUMBER=substr(Z103_REC_KEY_1,6,9)
+and Z103_LKR_TYPE='ADM'
+and substr(Z103_REC_KEY_1,1,5)='FCL01'
+and substr(Z00R_FIELD_CODE,0,3) ='FMT'
+) rr
+    on substr(ord1.Z68_REC_KEY ,0, 9) = rr.ADM_N
+    WHERE
+    rr.z00r_text like 'MU%'
+    and substr(ord1.z68_material_type,0,1 )= 'A'
+    and ord1.z68_order_type= 'M'
+    and  ord1.Z68_OPEN_DATE < 20200630
+    and ord1.Z68_ORDER_STATUS  = 'LC')
+    or
+--restriction 6
+ord.Z68_REC_KEY not in (
+    SELECT ord1.Z68_REC_KEY FROM
+    AMH50.Z68 ord1
+    inner join (
+    select Z00R_DOC_NUMBER, Z00R_TEXT, substr(Z103_REC_KEY,6,9) as ADM_N from AMH50.Z103, FCL01.Z00R
+where Z00R_DOC_NUMBER=substr(Z103_REC_KEY_1,6,9)
+and Z103_LKR_TYPE='ADM'
+and substr(Z103_REC_KEY_1,1,5)='FCL01'
+and substr(Z00R_FIELD_CODE,0,3) ='FMT'
+) rr
+    on substr(ord1.Z68_REC_KEY ,0, 9) = rr.ADM_N
+    WHERE
+    rr.z00r_text like 'MU%'
+    and substr(ord1.z68_material_type,0,1 )= 'A'
+    and ord1.z68_order_type= 'M'
+    and  ord1.Z68_OPEN_DATE < 20200101
+    and ord1.Z68_ORDER_STATUS  = 'SV')
     )
