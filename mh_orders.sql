9c755fb30f5aa390a24c47cacf0e55eb6b33f989
Aaron Neslin <aneslin125@gmail.com>
2020-10-01 12:25:38 -0400

    Create mh_orders.sql

---
+++ mh_orders.sql
@@ -0,0 +1,11 @@
+Select * from MHC50.Z68 ord
+WHERE
+ord.Z68_ORDER_STATUS = 'SV'
+OR
+(
+ord.Z68_ORDER_TYPE in ('O', 'S')
+and
+ord.Z68_ORDER_STATUS ='LC'
+and
+ord.Z68_ORDER_STATUS_DATE_X >= 20180701
+)
